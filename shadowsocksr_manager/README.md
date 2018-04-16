79f1f96be2dab544acb941a180a0e65987247f28
dhg <dhg___dhg@163.com>
2018-04-16 11:51:14 +0800

    较大改变

--- shadowsocksr_manager/README.md
+++ shadowsocksr_manager/README.md
@@ -3,7 +3,80 @@
 3. 功能
     - 查看连接IP
     - 重启 关闭 状态
-    - 修改密码(只是写成出了步骤)
-4. 截图 
+    - 修改配置
+4. 部分截图 
     - ![](./example.png)
+5. 运行示例:
+    - 
+        ```
+        [root@XXX ~]# ./manager.sh 
+
+            please select your option
+            1 show connectted ip and ip information
+            2 chang server config
+            3 start server
+            4 stop server
+            5 restart server
+            6 show server status
+            0 quit
+            
+        please input [default is 0]: 1
+        show verbose information, please input y or n [n]:
+        100.100.100.100      China XXX XXX             
+        100.100.100.100      China XXX XXX               
+        please input [default is 0]: 1
+        show verbose information, please input y or n [n]:y
+        100.100.100.100
+            status: success
+            city: XXX
+            zip: 
+            countryCode: CN
+            country: China
+            region: SD
+            isp: China XXX XXX
+            lon: 12
+            timezone: Asia/Shanghai
+            as: XXX
+            query: 100.100.100.100
+            lat: 11.0
+            org: China XXX XXX
+            regionName: XXX
+
+
+        100.100.100.100
+            status: success
+            city: XXX
+            zip: 
+            countryCode: CN
+            country: China
+            region: SD
+            isp: China XXX XXX
+            lon: 78.3333
+            timezone: Asia/Shanghai
+            as: AS
+            query: 100.100.100.100
+            lat: 68
+            org: China XXX
+            regionName: XXX
+
+
+        please input [default is 0]: 2
+        Do you change your port [N/y]y
+        please input port
+        for example:
+            8080,8978
+        your ports:8880, 8090
+        IPv6 support
+        2018-04-16 03:40:01 INFO     shell.py:72 ShadowsocksR 3.4.0 2017-07-27
+        stopped
+        ShadowsocksR stop success
+        IPv6 support
+        2018-04-16 03:40:01 INFO     util.py:85 loading libsodium from libsodium.so.18
+        2018-04-16 03:40:01 INFO     shell.py:72 ShadowsocksR 3.4.0 2017-07-27
+        started
+        ShadowsocksR start success
+        change success.
+        please input [default is 0]:        
+        ```
+
 5. ssr配置:https://www.qcgzxw.cn/1640.html/comment-page-64
--- shadowsocksr_manager/manager.py
+++ shadowsocksr_manager/manager.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
-#coding = utf-8
+# coding = utf-8
 
 import urllib2
 import os
 import json
 import re
-# set allow ip in allow_ip_list, for example: allow_ip_list = ['192.168.111.1', '12.167.2.1']
-allow_ip_list = ['112.38.217.35', '223, 104.186.2']
 
 
 def get_ip(is_verbose):
@@ -15,47 +13,53 @@ def get_ip(is_verbose):
         content = urllib2.urlopen(api_url).read()
         json_data = json.loads(content)
         if is_verbose != 'y':
-            print '{0}{1} {2} {3}{4}'.format(ip.ljust(20), json_data['country'], json_data['regionName'], json_data['city'].ljust(20), ip in allow_ip_list)
+            print '{0}{1} {2} {3}'.format(ip.ljust(20), json_data['country'], json_data['regionName'], json_data['city'].ljust(20))
         else:
             print ip
-            print '\tallow: {0}'.format(ip in allow_ip_list)
             for i, v in json_data.items():
                 print '\t{0}: {1}'.format(i, v)
             print '\n'
-    str = os.popen('netstat -anp|grep python|grep ESTABLISHED').read()
+    str=os.popen('netstat -anp|grep python|grep ESTABLISHED').read()
     for i in set(set(re.findall('.+f:(.+):.*ES', str))):
         get_ip_inform(i)
 
+def change_config():
+    os.system('vi /etc/shadowsocks.json')
+    if raw_input('Do you change your port [N/y]') == 'y':
+        print 'please input port'
+        print 'for example:\n\t8080,8978'
+        ports_list=raw_input('your ports:').replace(' ', '').split(',')
+        for port in ports_list:
+            os.system(
+                'iptables -I INPUT -p tcp --dport {0} -j ACCEPT'.format(port))
+    os.system('/etc/init.d/shadowsocks restart')
+    print 'change success.'
 
 def main():
-    str = '''
+    str='''
     please select your option
     1 show connectted ip and ip information
     2 chang server config
     3 start server
     4 stop server
     5 restart server
-    6 show server status 
+    6 show server status
     0 quit
     '''
     print str
-    number = 0
+    number=0
     while(True):
         try:
-            number = int(raw_input("please input [default is 0]: "))
+            number=int(raw_input("please input [default is 0]: "))
         except ValueError:
-            number = 0
+            number=0
         if number == 0:
             exit()
         elif number == 1:
-            if not allow_ip_list:
-                print 'recommand set allow ip in this file'
             get_ip(
                 raw_input('show verbose information, please input y or n [n]:'))
         elif number == 2:
-            print 'vi /etc/shadowsocks.json'
-            print 'vi /etc/sysconfig/iptables'
-            print 'reboot this machine'
+            change_config()
         elif number == 3:
             os.system('/etc/init.d/shadowsocks start')
         elif number == 4:
